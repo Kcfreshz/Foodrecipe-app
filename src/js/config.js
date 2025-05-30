c6288711901becf89899a780ba6a2b816d94e380
kcfreshz <kckc176@gmail.com>
2025-05-30 16:51:19 +0100

    Initial Commit: Foodrecipe-app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a092bd9e-43e7-40d7-bf61-d39c9c14e5e3';
+export const MODAL_CLOSE_SEC = 2.5;
